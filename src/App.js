26793d52a6ecfadc955cbdbc32eef294df785555
levilu800b <levilu800b@gmail.com>
2021-01-19 11:09:55 +0000

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Add from "./Add";
+import Links from "./Links";
+import VisibleItemList from "./VisibleItemList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Navbar bg="info" variant="dark">
+          <Navbar.Brand>React Shopping List</Navbar.Brand>
+        </Navbar>
+        <Container>
+          <Add/>
+          <Links/>
+          <VisibleItemList/>
+        </Container>
+      </>
+    );
+  }
 }
 
 export default App;
